0283fbdaa4f89ec389d8fba559a4644ec826d983
pbsmith82 <pbsmith82@gmail.com>
2021-04-04 20:46:24 -0400

    commit tree 9f3a6b5e8d54cb2399843099c4133f8386ee8427

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, name){
+    return array.filter(x => new RegExp(name, 'i').test(x))
+}
+
+function fuzzyMatch(array, term){
+   
+    return array.filter(x => (x.startsWith(term)))
+}
+
+function matchName(array, name){
+    return array.filter(x => (x.name === name) )  
+}
